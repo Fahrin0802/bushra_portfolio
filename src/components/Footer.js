7c7a002af4876b52c1232cee203f80af7a26301a
Fahrin Bushra <87880319+Fahrin0802@users.noreply.github.com>
2024-07-22 19:35:33 -0600

    Portfolio V1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ export const Footer = () => {
         <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
             <Layout className='py-8 flex items-center justify-between'>
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-                <Link href="/">Say hello</Link>
             </Layout>
         </footer>
     )
